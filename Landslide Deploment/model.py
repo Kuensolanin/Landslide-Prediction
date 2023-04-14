2df83d193972dc3e845c439028b7a577c9e5b80e
KuenSolanin <kuensolanin@users.noreply.github.com>
2023-04-14 14:32:43 +0600

    fixing workflow

--- Landslide Deploment/model.py
+++ Landslide Deploment/model.py
@@ -19,7 +19,7 @@ y = db['Code']
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, stratify=y)
 X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.125, random_state=42, stratify=y_train) #stratify is used for keepting the percentage ratio same while taking sampled from testing
 
-rf = RandomForestClassifier(random_state=0)
+rf = RandomForestClassifier(random_state=20)
 rf.fit(X_train, y_train)
 
 val_accuracy = []
